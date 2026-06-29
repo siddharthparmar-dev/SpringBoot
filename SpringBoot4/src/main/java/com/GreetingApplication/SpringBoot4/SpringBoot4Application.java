4af42bcaeefa30de70456807edeeb776b4752473
Siddharth <siddharthparmar.dev@gmail.com>
2026-06-29 23:47:25 +0530

    Added SpringBoot4

---
+++ SpringBoot4/src/main/java/com/GreetingApplication/SpringBoot4/SpringBoot4Application.java
@@ -0,0 +1,13 @@
+package com.GreetingApplication.SpringBoot4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot4Application.class, args);
+	}
+
+}
---
+++ SpringBoot4/src/test/java/com/GreetingApplication/SpringBoot4/SpringBoot4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GreetingApplication.SpringBoot4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
